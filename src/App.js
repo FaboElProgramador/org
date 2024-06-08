7837185a36e9eab5eaa59606133e886c496d1938
Fabian Vazquez <fabo.rp@gmail.com>
2024-06-08 20:23:47 -0300

    Empeando react

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './componentes/Header/Header';
+console.log(Header)
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hola mundo con React
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Aprende React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
